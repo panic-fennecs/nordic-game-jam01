82e09bfabaa0b79ce8a142d7198867f91a34a109
memoryleak47 <memoryleak47@protonmail.ch>
2019-04-28 00:57:42 +0200

    started implementing eat the new way

--- nordic_game_jam/eat/Eat.gd
+++ nordic_game_jam/eat/Eat.gd
@@ -3,86 +3,71 @@ extends Node2D
 onready var im = get_node("/root/Main/InputManagerNode")
 onready var bm = get_node("/root/Main/UILayer/ButtonManagerNode")
 
-const MAX_NOTE_DIST = 20;
-const THRESHOLD = 200;
-const N = 4;
+const STOP_TIME = 1000;
+const THRESHOLD = 300;
+const BLACKLIST_LEN = 10;
 
-var patterns = null;
-var active = false;
-
-func _ready():
-	get_node("Character1/AnimationPlayer").play("drink")
-	get_node("Character2/AnimationPlayer").play("eat")
-	get_node("Character1").set_scale(Vector2(0.5, 0.5))
-	get_node("Character2").set_scale(Vector2(0.5, 0.5))
-
-func generate_random(n, start_timestamp, possible_keys):
-	var inst = load("res://patterns/Pattern.gd")
-	return inst.generate_random(n, start_timestamp, null, 1000, possible_keys);
+var blacklist = []
+var composer = 0
+var state = "compose"
+var current_pattern = []
+var current_full_pattern = null
+var active = false
 
 func restart():
-	bm.clear_preps();
-	var raw_pattern = generate_random(N, 0, ["up", "left", "right", "down"]);
-	patterns = [[], []];
-	for i in range(0, N):
-		patterns[i % 2].append(raw_pattern.hits[i]);
-		bm.prepare_key(str(i % 2), raw_pattern.hits[i].key, raw_pattern.hits[i].timestamp);
-	im.clear()
+	randomize()
+	composer = 0 # randi() % 2 # TODO
+	current_pattern = []
+	current_full_pattern = []
+	state = "compose"
 
 func on_gain_focus():
-	active = true;
+	active = true
 	restart()
 
 func on_lose_focus():
-	active = false;
-	patterns = null;
-	im.clear();
-	bm.clear_preps();
+	active = false
 
-func fail():
-	print("fail!")
-	restart()
+func info(x):
+	print("info: ", x)
 
-func win():
-	print("win!")
-	im.clear();
-	patterns = null;
-	bm.clear_preps();
-	get_node("/root/Main").next_scene()
+func blacklisted(pattern):
+	return false
 
 func _process(_delta):
-	if not active:
-		return
-	
-	for p in [0, 1]:
-		var i = im.get_inputs(p);
-		if len(i) > 0 and patterns != null:
-			if i[0].input != patterns[p][0].key:
-				print("wrong key!")
-				if p == 0:
-					$Character1.spawn_emote("wrong")
-				else:
-					$Character2.spawn_emote("wrong")
-				fail();
+	if not active: return
+
+	if state == "compose":
+		var i = im.get_inputs(composer)
+		if len(i) > 0:
+			var first = i.pop_front();
+			current_pattern.append(first)
+			print("note added!")
+		elif len(current_pattern) > 0 and current_pattern.back().time < im.get_current_time() - STOP_TIME:
+			if blacklisted(current_pattern):
+				info("not that again!")
+				restart()
 				return
-			if abs(i[0].time - patterns[p][0].timestamp) >= THRESHOLD:
-				print("thresh!")
-				if p == 0:
-					$Character1.spawn_emote("rested")
-				else:
-					$Character2.spawn_emote("rested")
-				fail();
+			print("repeat now!")
+			state = "repeat"
+			current_full_pattern = current_pattern.duplicate(true)
+	elif state == "repeat":
+		var i = im.get_inputs(1 - composer)
+		if len(i) > 0:
+			var first = i.pop_front();
+			if current_pattern[0].input != first.input:
+				info("wrong note!")
+				restart()
 				return
-			bm.preps[bm.to_id(str(p), i[0].input)].pop_front();
-			i.pop_front();
-			patterns[p].pop_front();
-			print("small win")
-			$Character1.spawn_emote("love")
-			$Character2.spawn_emote("love")
-			if len(patterns[0]) == 0 and len(patterns[1]) == 0:
-				win();
-	
-		var ct = im.get_current_time()
-		if patterns != null and len(patterns[p]) > 0 and patterns[p][0].timestamp <= ct - THRESHOLD:
-			print("time fail!")
-			fail()
+			if abs(current_pattern[0].time - first.time) > THRESHOLD:
+				info("too far away!")
+				restart()
+				return
+			current_pattern.pop_front();
+			if len(current_pattern) == 0:
+				blacklist.append(current_full_pattern);
+				if len(blacklist) > BLACKLIST_LEN:
+					blacklist.pop_front(); 
+				info("nice!")
+				get_node("/root/Main").next_scene()
+				return
