146d09cda89cc79037d2e01c5f7caf436a4ec529
Philipp <philippboensch@gmail.com>
2019-04-27 11:20:00 +0200

    fixed eat scene and game crash

--- nordic_game_jam/Main.gd
+++ nordic_game_jam/Main.gd
@@ -12,14 +12,14 @@ func preload_scenes():
 	var eat_scene_resource = load("res://eat_scene/EatScene.tscn")
 	scene_list.append(eat_scene_resource)
 	
-func instantiate_scenes_current_iteration(var iteration):
+func instantiate_scenes_current_iteration(iteration):
 	#test loop for instantiating multiple scenes
 	for i in range(0, iteration):
 		instantiate_next_scene(i)
 		
 	iteration+=1
 		
-func instantiate_next_scene(var index):
+func instantiate_next_scene(index):
 	var next_scene = scene_list[0].instance()
 	next_scene.set_position(Vector2(0, index * 1080))
 	add_child(next_scene)
