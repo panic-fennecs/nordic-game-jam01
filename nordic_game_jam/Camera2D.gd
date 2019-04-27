874a609e49e6ffb4ee65f7bd196817f8f6b1e52a
Robin Wegner-Repke <wegner.robin@yahoo.de>
2019-04-27 11:23:54 +0200

    minor tweaks

--- nordic_game_jam/Camera2D.gd
+++ nordic_game_jam/Camera2D.gd
@@ -1,8 +1,10 @@
 extends Camera2D
 
+onready var tween 
+
 func _unhandled_key_input(event):
 	if event.pressed and event.scancode == KEY_3:
 		move_to_next_scene()
 		
 func move_to_next_scene():
-	self.set_position(Vector2(0,self.position.y + 1080))
+	self.set_position(Vector2(0, self.position.y + 1080))
--- nordic_game_jam/Main.gd
+++ nordic_game_jam/Main.gd
@@ -6,13 +6,13 @@ var scene_list = []
 
 func _ready():
 	preload_scenes()
-	instantiate_scenes_current_iteration(iteration)
+	instantiate_scenes_current_iteration()
 	
 func preload_scenes():
 	var eat_scene_resource = load("res://eat_scene/EatScene.tscn")
 	scene_list.append(eat_scene_resource)
 	
-func instantiate_scenes_current_iteration(var iteration):
+func instantiate_scenes_current_iteration():
 	#test loop for instantiating multiple scenes
 	for i in range(0, iteration):
 		instantiate_next_scene(i)
