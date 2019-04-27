1a27e2d789b9c21840bf362df91d50026d04d03c
memoryleak47 <memoryleak47@protonmail.ch>
2019-04-27 14:49:06 +0200

    fixed Button

--- nordic_game_jam/sleep/Sleep.gd
+++ nordic_game_jam/sleep/Sleep.gd
@@ -4,7 +4,10 @@ func generate_random(n, start_timestamp, possible_keys):
 	var inst = load("res://patterns/Pattern.gd")
 	return inst.generate_random(n, start_timestamp, null, 0, possible_keys);
 
-func _ready():
+func on_gain_focus():
 	var bm = get_node("/root/Main/UILayer/ButtonManagerNode")
 	var pattern = generate_random(1, 0, ["up", "left", "right", "down"]);
 	bm.prepare_pattern("0", pattern); # TODO player 1
+
+func on_lose_focus():
+	pass
