44fdfad8ee9ccd98030ea0a8e1f53e5be1f22537
memoryleak47 <memoryleak47@protonmail.ch>
2019-04-28 03:00:12 +0200

    added markers to eat stage

--- nordic_game_jam/eat/Eat.gd
+++ nordic_game_jam/eat/Eat.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var im = get_node("/root/Main/InputManagerNode")
 onready var bm = get_node("/root/Main/UILayer/ButtonManagerNode")
+onready var sprite_prefab = load("eat/Marker.tscn")
 
 const BLACKLIST_THRESHOLD = 200;
 const BLACKLIST_LEN = 10;
@@ -11,10 +12,14 @@ var blacklist = []
 var current_pattern = []
 var waiting_for_player = null
 var active = false
+var pattern_visuals = []
 
 func restart():
 	current_pattern = []
 	waiting_for_player = null
+	for x in pattern_visuals:
+		x.queue_free()
+	pattern_visuals = []
 
 func on_gain_focus():
 	active = true
@@ -53,6 +58,12 @@ func _process(_delta):
 				var x = i.pop_front();
 				current_pattern.append({"input": x.input, "time": im.get_current_time()})
 				waiting_for_player = 1 - p
+				var visu = sprite_prefab.instance();
+				visu.position = Vector2(80 + len(pattern_visuals) * 80, 80);
+				visu.play(x.input);
+				pattern_visuals.append(visu);
+				add_child(visu);
+				return
 	else:
 		if len(im.get_inputs(1 - waiting_for_player)) > 0:
 			info("didn't react!")
